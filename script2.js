5b7daf5554db8e75c5302173907fa3b147b0452e
aurelie-castro <acdemarly@gmail.com>
2020-11-13 00:08:01 +0100

    changed it to one setinterval

--- script2.js
+++ script2.js
@@ -5,69 +5,64 @@ import $ from 'jquery';
 
 
 setInterval (function(){
-    var rand = Math.floor(Math.random() *2);
-    console.log(rand);
-    var position = $(".square").position();
-    var topNow = position.top;
-    var leftNow = position.left;
-    console.log(topNow);
-    if (rand == "0"){
+    //premiere div
+    var rand1 = Math.floor(Math.random() *2);
+    var position1 = $(".square").position();
+    var topNow1 = position1.top;
+    var leftNow1 = position1.left;
+    if (rand1 == "0"){
         $(".square").css({
-            top: topNow,
-            left: leftNow
+            top: topNow1,
+            left: leftNow1
         });
-        console.log( rand + " purple n'avance pas");
-    }else if (rand == "1"){
+        console.log( rand1 + " purple n'avance pas");
+    }else if (rand1 == "1"){
         $(".square").css({
-            top: topNow,
-            left: leftNow + 100
+            top: topNow1,
+            left: leftNow1 + 100
         });
-        console.log(rand + " purple avance");
+        console.log(rand1 + " purple avance");
+    }
+    
+    //deuxieme div
+    var rand2 = Math.floor(Math.random() *2);
+    var position2 = $(".square2").position();
+    var topNow2 = position2.top;
+    var leftNow2 = position2.left;
+    if (rand2 == "0"){
+        $(".square2").css({
+            top: topNow2,
+            left: leftNow2
+        });
+        console.log( rand2 + " pink n'avance pas");
+    }else if (rand2 == "1"){
+        $(".square2").css({
+            top: topNow2,
+            left: leftNow2 + 100
+        });
+        console.log(rand2 + " pink avance");
+    }
+
+    //troisieme div
+    var rand3 = Math.floor(Math.random() *2);
+    var position3 = $(".square3").position();
+    var topNow3 = position3.top;
+    var leftNow3 = position3.left;
+    if (rand3 == "0"){
+        $(".square3").css({
+            top: topNow3,
+            left: leftNow3
+        });
+        console.log( rand3 + " blue n'avance pas");
+    }else if (rand3 == "1"){
+        $(".square3").css({
+            top: topNow3,
+            left: leftNow3 + 100
+        });
+        console.log(rand3 + " blue avance");
     }
 }, 1000); 
 
-setInterval (function(){
-    var rand = Math.floor(Math.random() *2);
-    console.log(rand);
-    var position = $(".square2").position();
-    var topNow = position.top;
-    var leftNow = position.left;
-    console.log(topNow);
-    if (rand == "0"){
-        $(".square2").css({
-            top: topNow,
-            left: leftNow
-        });
-        console.log(rand + " pink n'avance pas");
-    }else if (rand == "1"){
-        $(".square2").css({
-            top: topNow,
-            left: leftNow + 100
-        });
-        console.log( rand + " pink avance");
-    }
-}, 1000); 
 
-setInterval (function(){
-    var rand = Math.floor(Math.random() *2);
-    console.log(rand);
-    var position = $(".square3").position();
-    var topNow = position.top;
-    var leftNow = position.left;
-    console.log(topNow);
-    if (rand == "0"){
-        $(".square3").css({
-            top: topNow,
-            left: leftNow
-        });
-        console.log(rand + " blue n'avance pas");
-    }else if (rand == "1"){
-        $(".square3").css({
-            top: topNow,
-            left: leftNow + 100
-        });
-        console.log(rand + " blue avance");
-    }
-}, 1000); 
 
 
